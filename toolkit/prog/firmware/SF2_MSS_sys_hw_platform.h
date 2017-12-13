3617e147f949c1202a6a324ca1f803835c57a978
Michael John Klopfer <mklopfer@uci.edu>
2017-12-13 10:20:31 -0800

    Update Project with Programmer Template

---
+++ toolkit/prog/firmware/SF2_MSS_sys_hw_platform.h
@@ -0,0 +1,19 @@
+#ifndef SF2_MSS_sys_HW_PLATFORM_H_
+#define SF2_MSS_sys_HW_PLATFORM_H_
+/*****************************************************************************
+*
+*Created by Microsemi SmartDesign  Fri Dec 08 16:23:40 2017
+*
+*Memory map specification for peripherals in SF2_MSS_sys
+*/
+
+/*-----------------------------------------------------------------------------
+* CM3 subsystem memory map
+* Master(s) for this subsystem: CM3 
+*---------------------------------------------------------------------------*/
+#define COREPWM_0_0                     0x50000000U
+#define COREGPIO_0                      0x50001000U
+#define COREUARTAPB_0                   0x50002000U
+
+
+#endif /* SF2_MSS_sys_HW_PLATFORM_H_*/
