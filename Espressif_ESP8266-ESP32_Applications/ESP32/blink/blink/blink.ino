c008bc7aa6e740107e857a32218a1b4d782f92bc
Michael John Klopfer <mklopfer@uci.edu>
2017-12-13 10:12:57 -0800

    Update Project

---
+++ Espressif_ESP8266-ESP32_Applications/ESP32/blink/blink/blink.ino
@@ -0,0 +1,16 @@
+int ledPin = 5;
+
+void setup()
+{
+    pinMode(ledPin, OUTPUT);
+    Serial.begin(115200);
+}
+
+void loop()
+{
+    Serial.println("Hello, world!");
+    digitalWrite(ledPin, HIGH);
+    delay(500);
+    digitalWrite(ledPin, LOW);
+    delay(500);
+}
